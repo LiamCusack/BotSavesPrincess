03f702b6a147d20645f42457f6fa863b7b6031fa
Liam Cusack <LCusack8108@gmail.com>
2021-05-22 13:50:24 -0600

    Add bot test to include x and y attributes, add these attributes to box class, tests pass

--- Bot_Saves_Princess_1/lib/bot.rb
+++ Bot_Saves_Princess_1/lib/bot.rb
@@ -1,4 +1,7 @@
 class Bot
+  attr_reader :x, :y
   def initialize(grid)
+    @x = (grid.size - 1) / 2
+    @y = (grid.size - 1) / 2
   end
 end
--- Bot_Saves_Princess_1/spec/bot_spec.rb
+++ Bot_Saves_Princess_1/spec/bot_spec.rb
@@ -1,13 +1,16 @@
 require 'rspec'
 require './lib/bot'
+require './lib/grid'
 
 describe Bot do
   describe 'initialize' do
     it 'is an instance of bot' do
-      grid = ["---", "---", "---"]
+      grid = Grid.new(3, ["p--", "-m-", "---"])
       bot = Bot.new(grid)
 
       expect(bot).to be_a Bot
+      expect(bot.x).to eq(1)
+      expect(bot.y).to eq(1)
     end
   end
 end
