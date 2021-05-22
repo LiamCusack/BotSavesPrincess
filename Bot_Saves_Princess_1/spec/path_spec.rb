a60833354ba8e3e985be3ae31b801be1adebc2c3
Liam Cusack <LCusack8108@gmail.com>
2021-05-22 16:19:19 -0600

    Add initialization path test

---
+++ Bot_Saves_Princess_1/spec/path_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require './lib/bot'
+require './lib/grid'
+require './lib/princess'
+require './lib/path'
+
+describe Path do
+  describe 'initialize' do
+    it 'is an instance of path' do
+      grid = Grid.new(3, ["p--", "-m-", "---"])
+      bot = Bot.new(grid)
+      princess = Princess.new(grid)
+      path = Path.new(bot, grid, princess)
+
+      expect(path).to be_a Path
+
+      expect(path.bot).to eq(bot)
+      expect(path.bot.x).to eq(1)
+      expect(path.bot.y).to eq(1)
+
+      expect(path.grid).to eq(grid)
+      expect(path.grid.size).to eq(3)
+      expect(path.grid.grid).to eq(["p--", "-m-", "---"])
+
+      expect(path.princess).to eq(princess)
+      expect(path.princess.x).to eq(0)
+      expect(path.princess.y).to eq(0)
+    end
+  end
+end
