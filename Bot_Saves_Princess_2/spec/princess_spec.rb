8814c4192890ead7b2c1288a35bff45715f5aaaf
Liam Cusack <LCusack8108@gmail.com>
2021-05-22 19:19:27 -0600

    edit princess spec test to account for new requirements

--- Bot_Saves_Princess_2/spec/princess_spec.rb
+++ Bot_Saves_Princess_2/spec/princess_spec.rb
@@ -5,13 +5,13 @@ require './lib/grid'
 describe Princess do
   describe 'initialize' do
     it 'is an instance of princess' do
-      grid = Grid.new(3, ["p--", "-m-", "---"])
+      grid = Grid.new(5, ["-----", "-----", "p-m--", "-----", "-----"])
       princess = Princess.new(grid)
 
       expect(princess).to be_a Princess
       expect(princess.x).to eq(0)
-      expect(princess.y).to eq(0)
-      expect(princess.coords).to eq([0, 0])
+      expect(princess.y).to eq(2)
+      expect(princess.coords).to eq([0, 2])
     end
   end
 end
