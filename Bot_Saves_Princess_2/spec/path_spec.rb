3d25d5cfef6f37d695d54dcb4752f9c56e773d0b
Liam Cusack <LCusack8108@gmail.com>
2021-05-22 19:25:11 -0600

    update path spec to reflect new requirements

--- Bot_Saves_Princess_2/spec/path_spec.rb
+++ Bot_Saves_Princess_2/spec/path_spec.rb
@@ -6,8 +6,8 @@ require './lib/path'
 
 describe Path do
   before :each do
-    @grid = Grid.new(3, ["p--", "-m-", "---"])
-    @bot = Bot.new(@grid)
+    @grid = Grid.new(5, ["-----", "-----", "p-m--", "-----", "-----"])
+    @bot = Bot.new(2, 2)
     @princess = Princess.new(@grid)
     @path = Path.new(@bot, @grid, @princess)
   end
@@ -19,42 +19,16 @@ describe Path do
       expect(@path).to be_a Path
 
       expect(@path.bot).to eq(@bot)
-      expect(@path.bot.x).to eq(1)
-      expect(@path.bot.y).to eq(1)
+      expect(@path.bot.x).to eq(2)
+      expect(@path.bot.y).to eq(2)
 
       expect(@path.grid).to eq(@grid)
-      expect(@path.grid.size).to eq(3)
-      expect(@path.grid.grid).to eq(["p--", "-m-", "---"])
+      expect(@path.grid.size).to eq(5)
+      expect(@path.grid.grid).to eq(["-----", "-----", "p-m--", "-----", "-----"])
 
       expect(@path.princess).to eq(@princess)
-      expect(@path.princess.x).to eq(0)
+      expect(@path.princess.x).to eq(2)
       expect(@path.princess.y).to eq(0)
-    end
-  end
-
-  describe 'path to princess' do
-    it 'outputs the correct moves to get to the princess' do
-      expect(@path.path_to_princess).to eq(["LEFT", "UP"])
-    end
-  end
-
-  describe 'movement' do
-    it 'correctly updates bot coordinates to bring bot closer to princess' do
-      expect(@bot.updated_coords).to eq([1, 1])
-
-      @path.movement
-
-      expect(@bot.updated_coords).to eq([0, 1])
-    end
-  end
-
-  describe 'output' do
-    it 'prints the current output_moves' do
-      expect(@path.output).to eq([])
-
-      @path.path_to_princess
-
-      expect(@path.output).to eq(["LEFT", "UP"])
     end
   end
 end
